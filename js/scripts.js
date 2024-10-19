26c72e124ad53dec50c815db1c3320eafc94681b
victormatabanchoy <143300687+victormatabanchoy@users.noreply.github.com>
2024-10-18 20:11:15 -0500

    barras de progreso animada

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ const toggleTheme = document.getElementById("toggle-theme");
 const toggleIcon = document.getElementById("toggle-icon");
 const toggleText = document.getElementById("toggle-text");
 
+const togglerColors = document.getElementById("toggle-colors");
+const rootStyles = document.documentElement.style;
+
 toggleTheme.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
@@ -12,4 +15,7 @@ toggleTheme.addEventListener("click", () => {
     toggleIcon.src = "assets/icons/moon.png";
     toggleText.textContent = "Dark mode";
   }
+});
+togglerColors.addEventListener("click", (e) => {
+  rootStyles.setProperty("--primary-color", e.target.dataset.color);
 });
